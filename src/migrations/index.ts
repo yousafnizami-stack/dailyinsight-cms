398a0fcd463746fc6a0163854cfec7fa70a92cbf
getotogit <yousaf.nizami@gmail.com>
2026-05-30 17:21:13 +0500

    fix: remove deleted migration reference from index.ts

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,12 +1,5 @@
-import * as migration_20260530_110132 from './20260530_110132';
 import * as migration_20260530_121458 from './20260530_121458';
-
 export const migrations = [
-  {
-    up: migration_20260530_110132.up,
-    down: migration_20260530_110132.down,
-    name: '20260530_110132',
-  },
   {
     up: migration_20260530_121458.up,
     down: migration_20260530_121458.down,
